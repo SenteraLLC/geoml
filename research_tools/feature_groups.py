b8b69f441b68fcbdd2b167730ab8eac8da2a32ae
tnigon <nigo0024@umn.edu>
2020-04-16 15:52:36 -0500

    feature #10 - create list of df col headings to build X and y

---
+++ research_tools/feature_groups.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 16 14:22:39 2020
+
+TRADE SECRET: CONFIDENTIAL AND PROPRIETARY INFORMATION.
+Insight Sensing Corporation. All rights reserved.
+
+@copyright: © Insight Sensing Corporation, 2020
+@author: Tyler J. Nigon
+@contributors: [Tyler J. Nigon]
+"""
+
+cs_test = [
+    'dae', 'rate_ntd_kgha', '460', '510', '560', '610', '660', '680', '710',
+    '720', '740', '760', '810', '870', '900']
+
+
