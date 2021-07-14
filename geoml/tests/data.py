893ac5769bc914937022cbb991dd6c134e62aab9
tnigon <nigo0024@umn.edu>
2021-07-14 17:54:35 -0500

    test files import geoml instead of research_tools

--- geoml/tests/data.py
+++ geoml/tests/data.py
@@ -19,7 +19,7 @@ base_dir_data = config.config_dict['Tables']['base_dir_data']
 fname_obs_tissue = os.path.join(
     base_dir_data, config.config_dict['Tables']['table_names']['obs_tissue'])
 fname_cropscan = os.path.join(
-    base_dir_data, config.config_dict['Tables']['table_names']['rs_cropscan'])
+    base_dir_data, config.config_dict['Tables']['table_names']['rs_cropscan_res'])
 
 df_obs_tissue = pd.read_csv(fname_obs_tissue)
 
