ca1e280b311cf39a3ce7aa4e9eac2985309def5b
Joel Larson <joel.larson@sentera.com>
2021-11-30 15:53:50 -0600

    Add config types and config file

---
+++ geoml/config.py
@@ -0,0 +1,56 @@
+from typing import TypedDict, Dict, Union, Set, Tuple, List, Optional, Literal, Any
+
+
+class DBConfig(TypedDict):
+    name   : str
+    host   : str
+    user   : str
+    schema : str
+    port   : int
+
+
+class TableConfig(TypedDict):
+    database    : DBConfig
+    table_names : Dict[str, str]
+
+
+class GroupFeatures(TypedDict):
+    dae                 : str
+    dap                 : str
+    rate_ntd            : Dict[str, str]
+    sentinel_wl_range   : Tuple[int, int]
+    weather_derived     : List[str]
+    weather_derived_res : List[str]
+    bands               : List[str]
+
+
+class FeatureDataConfig(TypedDict):
+    random_seed          : int
+    dir_results          : Optional[str]
+    group_feats          : GroupFeatures
+    ground_truth_tissue  : str
+    ground_truth_measure : str
+    date_tolerance       : int
+    # TODO: These kwarg arguments can be much more flexible
+    cv_method_kwargs     : Dict[str, int]
+    cv_split_kwargs      : Dict[str, str]
+    impute_method        : str
+    train_test           : str
+    cv_split_tune_kwargs : Dict[Literal["y"], List[str]]
+    print_out_fd         : bool
+    print_spliiter_info  : bool
+
+
+class FeatureSelectionConfig(TypedDict):
+    model_fs                   : Any
+    model_fs_params_set        : Dict[str, Any]
+    model_fs_params_adjust_min : Dict[str, Any]
+    model_fs_params_adjust_max : Dict[str, Any]
+    n_feats                    : int
+    n_linspace                 : int
+
+
+class Config(TypedDict):
+    table             : TableConfig
+    feature_data      : FeatureDataConfig
+    feature_selection : FeatureSelectionConfig
